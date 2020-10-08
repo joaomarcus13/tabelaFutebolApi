8d34f09371dc70152ef034cbccdb6d1f357d1ae7
joao <joaomarcus13@hotmail.com>
2020-10-08 17:20:07 -0300

    changes

--- src/server.js
+++ src/server.js
@@ -30,6 +30,6 @@ app.get('/tabela/:nomeCampeonato',verify,tabelaController )
 app.get('/jogos/:nomeCampeonato',verify,jogosController )
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('server port 3000')
 })
